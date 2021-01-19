de16b1466fb0f41f7c0c6a1bfee0b13d7b94f97c
Shawn Blanchard <shawn_blanchard@schumachergroup.com>
2021-01-19 15:43:00 -0600

    api-extractor

--- src/index.ts
+++ src/index.ts
@@ -21,11 +21,12 @@ export function avg(a: number, b: number, c: number): number {
  * @param a - first number
  * @param b - second number
  * @param c - third number
+ * @param d - fourth number
  *
  * @beta
  */
-export function sum3(a: number, b: number, c: number): number {
-  return sum2(a, sum2(b, c));
+export function sum3(a: number, b: number, c: number, d = 0): number {
+  return sum2(a, b) + sum2(c, d);
 }
 
 /**
